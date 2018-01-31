c2e3d07805378a2c0a3305d9a79adeecba1c2b42
582302894 <582302894@qq.com>
2018-01-31 09:50:48 +0800

    test

--- test.php
+++ test.php
@@ -1,9 +1,3 @@
 <?php
 
-
-$url = 'http://127.0.0.1/web/baidu/yingxiao/v2/home/product/details/id/685';
-
-for ($i = 0; $i < 10; $i++) {
-    file_get_contents($url);
-}
-die('die');
+var_dump(is_numeric('1111111111111111111111111111111111'));
