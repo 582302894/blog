5de1032c0fa49363070555fba0c032d9192f1c33
582302894 <582302894@qq.com>
2018-02-26 09:53:45 +0800

    test

--- test.php
+++ test.php
@@ -1,29 +1,6 @@
 <?php
 
-$data = [
-    'a' => 'b',
-];
-$data = http_build_query($data);
-$cookie = [
-    'aaa' => 'aaa',
-];
-$cookie = http_build_query($cookie);
-$opts = [
-    'http' => [
-        'method' => 'POST',
-        'header' =>
-        "Content-type: application/x-www-form-urlencoded\r\n" .
-        "Cookie: " . $cookie . "\r\n" .
-        "Content-Length:" . strlen($data) . "\r\n",
-        'content' => $data,
-    ],
-];
+$res = stream_get_wrappers();
 
-$context = stream_context_create($opts);
-
-$url = 'http://www.hanzi.com/test.php';
-$result = file_get_contents($url, false, $context);
-echo '<meta charset=utf-8><pre>';
-var_dump(get_headers($url));
-var_dump($result);
-echo '</pre>';exit();
+echo "<pre>";
+print_r($res);
