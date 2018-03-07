666bb0775b2a10ec267bce565f9b92196e0a7ea4
582302894 <582302894@qq.com>
2018-03-07 18:17:17 +0800

    输出流导出量级数据

--- lib/app/index/index.php
+++ lib/app/index/index.php
@@ -2,11 +2,23 @@
 namespace lib\app\index;
 
 class index {
-	
+
     public function index() {
     }
 
     public function test() {
         return 'test';
+    }
+
+    public function output() {
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment; filename="test.csv"');
+        header('Cache-Control: max-age=0');
+        $fp = fopen('php://output', 'w');
+        $str = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
+        for ($i = 0; $i < 1000000; $i++) {
+            fputcsv($fp, array($str, $str, $str, $str, $str, $str));
+        }
+        fclose($fp);
     }
 }
